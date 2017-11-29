770e81230e6d8654c1cba1453c1fcf134eb9f8e8
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2017-11-29 11:23:12 -0800

    Contact form development.

---
+++ resources/js/contact.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  console.log("jQuery form 1");
+  $("input.name").change(function(){
+    console.log
+  })
+//  $("a").click(function(event){
+  //
+//    event.preventDefault();
+
+//    $(this).hide("slow");
+//  });
+});
