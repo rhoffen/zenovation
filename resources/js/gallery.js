f62866d023de74ade9ee94b8d699260c4f359dc4
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2017-11-30 13:35:06 -0800

    Gallery carousel in progress.

---
+++ resources/js/gallery.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    dots: true,
+    arrows: false
+    // adaptiveHeight: true
+  });
+});
